d8ad909bcc03a982ce325208e02ca1f19a9f2bf1
Ajay-0080 <ajaykp6@gmail.com>
2024-08-31 16:17:19 +0530

    first-commit

---
+++ practice_11.java
@@ -0,0 +1,13 @@
+ class practice_11 {
+    public static void main(String args[]){
+
+        StringBuffer sb=new StringBuffer("jenny");
+        System.out.println(sb.capacity());
+        sb.append("khatri");
+        System.out.println(sb);
+        sb.insert(5,"java");
+        System.out.println(sb);
+
+    }
+    
+}
