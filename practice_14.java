a8eac20357e4043075b4975c51d07090437ce93f
Ajay-0080 <ajaykp6@gmail.com>
2024-08-31 14:55:59 +0530

    initial

---
+++ practice_14.java
@@ -0,0 +1,23 @@
+class Human{
+    private int age=15;
+    
+    public int getName(){
+
+        return age;
+
+    } 
+    }
+
+    
+    class practice_14{
+        public static void main(String args[]){
+             Human obj1= new Human();
+           int old=obj1.getName();
+           System.out.println(old);
+
+              
+    
+        }
+    }
+    
+
