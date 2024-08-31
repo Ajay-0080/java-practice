10e0c148e6cd2859f8a4e6c8b5de4007db2b1cf5
Ajay-0080 <ajaykp6@gmail.com>
2024-08-31 16:34:37 +0530

    first-commiting

---
+++ practice_5.java
@@ -0,0 +1,30 @@
+class Calculator {
+ 
+    public int add(int n1,int n2)
+    {
+        return n1+n2;
+    }
+    public int add(int n1,int n2, int n3)
+    {
+        return n1+n2+n3;
+    }
+
+    public double add(double n1,int n2)
+    {
+        return n1+n2;
+    }
+    
+}
+
+ class practice_5 {
+    
+    public static void main(String args[]) {
+
+        Calculator obj=new Calculator();
+        
+        int res= obj.add(3,4);
+        System.out.println(res);
+
+
+    }
+}
