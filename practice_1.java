075340e6000f431ae3590b5b9fe5c661579314ff
Ajay-0080 <ajaykp6@gmail.com>
2024-08-31 13:07:39 +0530

    first code pushing

---
+++ practice_1.java
@@ -0,0 +1,11 @@
+class practice_1 {
+    public static void main(String args[]) {
+     float a=5.2f;
+     long b=58541l;
+     boolean c= true;
+     System.out.println(a);
+     System.out.println(b);
+     System.out.println(c);
+
+    }
+}
