07f3d258658c0cdf45d98da9851f98dfdbe5c86d
Ajay-0080 <ajaykp6@gmail.com>
2024-10-03 23:45:45 +0530

    first commit

---
+++ NestedTry.java
@@ -0,0 +1,30 @@
+ class NestedTry {
+
+	public static void main(String[] args) {
+		try {
+			try {
+				int data=100/2;
+			}
+			catch (ArithmeticException ae){
+				ae.printStackTrace();
+			}
+			try {
+				int [] arr=new int [5];
+				arr[4]=7;
+			}
+			catch (ArrayIndexOutOfBoundsException ai) {
+				ai.printStackTrace();
+				
+			}
+			 String str = null;
+			    System.out.println(str.length()); 
+			
+		}
+		
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
+		System.out.println("rest of the code");
+	}
+
+}
