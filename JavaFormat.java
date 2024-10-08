1f004991f74e99a140b93cb7762f97f35ffeafdf
Ajay-0080 <ajaykp6@gmail.com>
2024-10-09 00:02:09 +0530

    first commit

---
+++ JavaFormat.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+class JavaFormat {
+
+	public static void main(String[] args) {
+		
+        System.out.printf("%s %n","luminar");
+        System.out.printf("%S' %n","LUminar");
+        System.out.printf("%15s %n","Total:");
+        System.out.printf("%d \n",10000);
+        double d =2.456436;
+        System.out.printf("decimal point upto 2 decimal places: "+String.format("%.2f",d));
+        
+        Date joindate = new Date();
+        System.out.println(joindate);
+        System.out.printf("hours %tH: minutes %tM: seconds %tS %n",joindate,joindate,joindate);
+        System.out.printf("%1$tA, %1$tB %1$ty %n",joindate);
+        System.out.printf("%1$td, %1$tm, %1$ty %n",joindate); 
+                                                            
+        
+	}
+
+}
